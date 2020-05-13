164c820b41e7ac3c56d337bad2ca97f67b5b6496
Bob Sardinia <shibdib@users.noreply.github.com>
2020-05-12 22:07:17 -0400

    include bat

--- steam.bat
+++ steam.bat
@@ -1,7 +1,11 @@
 @echo off
+
+set X_TOOLS="D:\Program Files (x86)\Steam\steamapps\common\X Tools\WorkshopTool.exe"
+
 for /d %%i in (shib*) do (
 cd "%%i"
-START "" "D:\Program Files (x86)\Steam\steamapps\common\X Tools\WorkshopTool.exe" publishx4 -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -preview "%cd%\%%i\steam\preview.jpg"
-START "" "D:\Program Files (x86)\Steam\steamapps\common\X Tools\WorkshopTool.exe" update -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -changenote ""
+START "" %X_TOOLS% publishx4 -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -preview "%cd%\%%i\steam\preview.jpg"
+START "" %X_TOOLS% update -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -changenote ""
 cd ..
+START "" "D:\Program Files\7-Zip\7z.exe" a %%i.zip %%i
 )
