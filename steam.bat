a813eecf6739011b63d20ab1e7936502f090c4b2
Bob S <shibdib@users.noreply.github.com>
2025-05-22 15:17:51 -0400

    Update bat

--- steam.bat
+++ steam.bat
@@ -1,11 +1,9 @@
-@echo off
-
-set X_TOOLS="D:\Program Files (x86)\Steam\steamapps\common\X Tools\WorkshopTool.exe"
+set X_TOOLS="F:\SteamLibrary\steamapps\common\X Tools\WorkshopTool.exe"
 
 for /d %%i in (shib*) do (
 cd "%%i"
 START "" %X_TOOLS% publishx4 -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -preview "%cd%\%%i\steam\preview.jpg"
 START "" %X_TOOLS% update -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -changenote ""
 cd ..
-START "" "D:\Program Files\7-Zip\7z.exe" a %%i.zip %%i
+START "" "E:\Apps\7-Zip\7z.exe" a %%i.zip %%i
 )
