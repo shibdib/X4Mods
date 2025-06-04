31247f018fcf64a4164600219643a159e78a9059
Bob S <shibdib@users.noreply.github.com>
2025-06-04 13:58:28 -0400

    update

--- steam.bat
+++ steam.bat
@@ -1,9 +1,12 @@
+:: Set these variables. X_TOOLS is where X_TOOLS is installed. ZIP is where 7z.exe is. FOLDER_PREFIX is what folders you want to check and zip (all my mods folders start with shib_)
 set X_TOOLS="F:\SteamLibrary\steamapps\common\X Tools\WorkshopTool.exe"
+set ZIP="E:\Apps\7-Zip\7z.exe"
+set FOLDER_PREFIX=shib*
 
-for /d %%i in (shib*) do (
+for /d %%i in (%FOLDER_PREFIX%) do (
 cd "%%i"
 START "" %X_TOOLS% publishx4 -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -preview "%cd%\%%i\steam\preview.jpg"
 START "" %X_TOOLS% update -path "%cd%\%%i" -buildcat -contentdef "%cd%\%%i\steam\content.xml" -changenote ""
 cd ..
-START "" "E:\Apps\7-Zip\7z.exe" a %%i.zip %%i
+START "" %ZIP% a %%i.zip %%i
 )
